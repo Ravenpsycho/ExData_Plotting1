392fcd71a797fb3ca0732ee6fa519f9b59842ed7
Mathieu Canales <lebibounet@gmail.com>
2019-09-10 22:24:24 +0200

    Completed code

--- Plot1.R
+++ Plot1.R
@@ -1,11 +1,17 @@
 ## Downloading and unzipping the file
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "file1.zip")
-unzip(zipfile = "file1.zip")
+if (!file.exists("household_power_consumption.txt")){
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "file1.zip")
+        unzip(zipfile = "file1.zip")
+        }
 dat <- read.csv2("household_power_consumption.txt", stringsAsFactors = F)
-head(dat)
 dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
 dat_sub <- dat[dat$Date == as.Date("2007-02-01") | dat$Date == as.Date("2007-02-02"), ]
+rm(dat)
 ## Plot 1 Code
+png("plot1.png", width = 480, height = 480)
+
 hist(as.numeric(dat_sub$Global_active_power), col = 2,
      main = "Global Active Power",
      xlab = "Global Active Power (Kilowatts)")
+
+dev.off()
--- Plot2.R
+++ Plot2.R
@@ -1,10 +1,24 @@
 ## Downloading and unzipping the file
-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "file1.zip")
-unzip(zipfile = "file1.zip")
+if (!file.exists("household_power_consumption.txt")){
+        download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "file1.zip")
+        unzip(zipfile = "file1.zip")
+}
 dat <- read.csv2("household_power_consumption.txt", stringsAsFactors = F)
-head(dat)
 dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
 dat_sub <- dat[dat$Date == as.Date("2007-02-01") | dat$Date == as.Date("2007-02-02"), ]
+dat_sub$Day <- weekdays(dat_sub$Date) 
+rm(dat)
 
 ## Plot 2 code
-plot(dat_sub$Global_active_power, type = "l", x_)
+png("plot2.png",width = 480, height = 480)
+
+first_fri <- which(dat_sub$Day =="Friday")[1]
+all(dat_sub$Day[1:1440] == "Thursday") & all(dat_sub$Day[1441:2880] == "Friday")
+plot(dat_sub$Global_active_power, 
+     type = "l", 
+     xaxt = "n", 
+     xlab = "Day",
+     ylab = "Global Active Power (kilowatts)")
+axis(1, at = c(0, first_fri, dim(dat_sub)[[1]]), labels = c("Thu", "Fri", "Sat"))
+
+dev.off()
